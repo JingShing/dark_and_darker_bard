a1b6adfa1a5eb13e2b6e2f50727e4497c13cda0e
JingShing <JingShingLai@gmail.com>
2023-08-30 13:15:27 +0800

    Update auto_play_bard.py

--- script/auto_play_bard.py
+++ script/auto_play_bard.py
@@ -1,13 +1,28 @@
 import pyautogui
 # pip install pyautogui
 target_image_path = 'image.png'
+global confidence_value
 confidence_value = 0.8
-while True:
+def read_first_line_as_float(file_path):
     try:
-        location = pyautogui.locateOnScreen(target_image_path)
-        if location is not None:
-            pyautogui.click(button='right', confidence=confidence_value)
-            print("play")
+        with open(file_path, 'r') as file:
+            first_line = file.readline().strip()
+            first_line_as_float = float(first_line)
+            return first_line_as_float
+    except:
+        return 1
+def detect_tempo():
+    global confidence_value
+    while True:
+        try:
+            location = pyautogui.locateOnScreen(target_image_path)
+            if location is not None:
+                pyautogui.click(button='right', confidence=confidence_value)
+                print("play")
 
-    except Exception as e:
-        print(f"error: {str(e)}")
+        except Exception as e:
+            print(f"error: {str(e)}")
+
+if __name__ == "__main__":
+    confidence_value = read_first_line_as_float("confidence.txt")
+    detect_tempo()
